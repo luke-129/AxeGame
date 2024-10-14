eed3740611b4624ad2bdb5167a5c42478e61aaad
Luke Fyfe <lukee129@outlook.com>
2024-10-14 18:00:18 +0800

    Draw a circle for prototyping

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ int main()
     int height = 350;
     int width = 350;
     InitWindow(width, height, "AxeGame");
-
-    while (true)
+    int circle_center_x = width / 2;
+    int circle_center_y = height / 2;
+    float circle_radius = 50.f;
+    while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RED);
+        DrawCircle(circle_center_x, circle_center_y, circle_radius, BLUE);
         EndDrawing();
     }
 }
